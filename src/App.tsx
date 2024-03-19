c841012078b343a18b23db0200fb2083aeb174ee
Buterbrod-iku <melkov750@gmail.com>
2024-03-19 16:10:57 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import SortTime from "./components/sortComponents/sortTime/sortTime.tsx";
 function App() {
     const [rand] = useState(parseInt(String(Math.random() * (10 - 1) + 1)))
     const [arrayBlock, setArrayBlock] = useState(Array.from({length: rand}, (item, index) => {
+        const i = item
+        console.log(i)
         const randomTime = parseInt(String(Math.random() * (25 - 3) + 3))
         return {id: index, startTime: randomTime, time: randomTime, color: hexGenerator()}
     }));
--- src/utils/onChangeHexColor.ts
+++ src/utils/onChangeHexColor.ts
@@ -1,4 +1,6 @@
-export const onChangeHexColor = (event, setInput, setCheck) => {
+import {ChangeEvent, Dispatch, SetStateAction} from "react";
+
+export const onChangeHexColor = (event: ChangeEvent<HTMLInputElement>, setInput: Dispatch<SetStateAction<string>>, setCheck: Dispatch<SetStateAction<boolean>>) => {
     const reg = /^#[0-9A-F]{6}[0-9a-f]{0,2}$/i
     const text = event.target.value
     setInput(text)
